759e8d2871b13dfb0f371dd6e7aff80d8837bedc
abdulRahman elshikh <abdulrahmanelshikh2001@gmail.com>
2024-02-29 00:44:52 +0200

    updates

--- src/components/ImgContainer.jsx
+++ src/components/ImgContainer.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
-import Container from './Container'
+import React from "react";
+import Container from "./Container";
 
-const ImgContainer = ({children, className}) => {
+const ImgContainer = ({ children, className , style }) => {
   return (
-    <Container className={`overflow-hidden m-0 p-0 rounded bg-primary img-container ${className}`}>
-        {children}
+    <Container
+      className={`overflow-hidden m-0 p-0 rounded bg-primary img-container ${className}`}
+      style={style}
+    >
+      {children}
     </Container>
-  )
-}
+  );
+};
 
-export default ImgContainer
+export default ImgContainer;
