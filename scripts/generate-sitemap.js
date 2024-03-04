f7809b1e5083f7d4ce317341da52985b3260a23b
abdulRahman elshikh <abdulrahmanelshikh2001@gmail.com>
2024-03-04 18:41:54 +0200

    test goggle search console

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -4,6 +4,57 @@ const { create } = require("xmlbuilder2");
 const urls = [
   { loc: "https://www.rakaya.sa/", lastmod: "2024-03-04", priority: 1.0 },
   { loc: "https://www.rakaya.sa/rakaya", lastmod: "2024-03-04", priority: 0.8 },
+  {
+    loc: "https://www.rakaya.sa/rakaya/our-pillars",
+    lastmod: "2024-03-04",
+    priority: 0.7,
+  },
+  {
+    loc: "https://www.rakaya.sa/rakaya/Our-work",
+    lastmod: "2024-03-04",
+    priority: 0.6,
+  },
+  {
+    loc: "https://www.rakaya.sa/rakaya/our-services",
+    lastmod: "2024-03-04",
+    priority: 0.5,
+  },
+  {
+    loc: "https://www.rakaya.sa/rakaya/our-products",
+    lastmod: "2024-03-04",
+    priority: 0.4,
+  },
+  {
+    loc: "https://www.rakaya.sa/rakaya/feedbacks",
+    lastmod: "2024-03-04",
+    priority: 0.4,
+  },
+  {
+    loc: "https://www.rakaya.sa/rakaya/applications",
+    lastmod: "2024-03-04",
+    priority: 0.4,
+  },
+  {
+    loc: "https://www.rakaya.sa/rakaya/sessions",
+    lastmod: "2024-03-04",
+    priority: 0.4,
+  },
+  {
+    loc: "https://www.rakaya.sa/rakaya-studio",
+    lastmod: "2024-03-04",
+    priority: 0.3,
+  },
+  { loc: "https://www.rakaya.sa/jobs", lastmod: "2024-03-04", priority: 0.4 },
+  {
+    loc: "https://www.rakaya.sa/our-way",
+    lastmod: "2024-03-04",
+    priority: 0.2,
+  },
+  {
+    loc: "https://www.rakaya.sa/contact-us",
+    lastmod: "2024-03-04",
+    priority: 0.1,
+  },
 ];
 
 const root = create({ version: "1.0", encoding: "UTF-8" }).ele("urlset", {
