cedab1e02ca045cfbf5af973537a0220fa241c70
abdulRahman elshikh <abdulrahmanelshikh2001@gmail.com>
2024-03-06 15:57:32 +0200

    test robot

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
     siteUrl:  'https://www.rakaya.sa/',
-    generateRobotsTxt: true, // (optional)
+    // generateRobotsTxt: true, // (optional)
     // ...other options
   }
