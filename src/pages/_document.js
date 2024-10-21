8dfd9d047c8d650a1c88d5648b8d5af0d9de62ae
abdulRahman elshikh <abdulrahmanelshikh2001@gmail.com>
2024-10-21 09:18:38 +0300

    test clarity

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -12,7 +12,7 @@ export default function Document() {
           }
           rel="stylesheet"
         />
-        <Script id="clarity" strategy="afterInteractive">
+        {/* <Script id="clarity" strategy="afterInteractive">
           {`
     (function(c,l,a,r,i,t,y){
       c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)}
@@ -20,7 +20,19 @@ export default function Document() {
       y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
     })(window, document, "clarity", "script", "oly0zjsgbl");
   `}
-        </Script>
+        </Script> */}
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments);};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "oly0zjsgbl");
+            `,
+          }}
+        />
       </Head>
       <body>
         <Main />
